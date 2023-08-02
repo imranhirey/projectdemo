2ca709e6acc275e1346f92f0c01c50cacdcade46
imranhirey <imrannurhirey@gmail.com>
2023-08-02 14:43:24 +0100

    my first

---
+++ src/components/Newpage.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Newpage() {
+  return (
+    <div>
+    <h1
+    style={{
+      color:"red",
+      fontSize:"50px",
+      textAlign:"center",
+      marginTop:"100px"
+
+    }}
+    >
+      I WILL PUT NEW PAGE HERE ACCORDING TO THE ORGANAZAYION NEEDS
+    </h1>
+    </div>
+  )
+}
+
+export default Newpage
